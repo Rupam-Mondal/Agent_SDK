f2b0d6e8e28466aa23819c5680fe8845b4158560
Rupam-Mondal <rupammondal564@gmail.com>
2026-07-31 16:56:54 +0530

    some chnages related to npm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import { Agent } from "./agent.js";
-
-
-const AIagent = await new Agent("gpt-4o-mini" , process.env.OPEN_AI_API_KEY)
-                .liveDataQueryRun("who is current pm of India ?" , process.env.TAVILY_API_KEY)
-
-console.log(AIagent);

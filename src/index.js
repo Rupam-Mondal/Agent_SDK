3df3fe9cead7b694e0d5f624b7ddc112deff82d4
Rupam-Mondal <rupammondal564@gmail.com>
2026-08-01 11:18:08 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import { Agent } from "./agent.js";
+export { Agent } from "./agent.js";
+export { handOff } from "./handoff.js";
 
-
-const emailAgent = new Agent("gpt-4o-mini", process.env.OPEN_AI_API_KEY);
-
-await emailAgent.sendEmail("dipankar7111@gmail.com", "Leave Application", "Write a formal leave application for two days due to illness.")
